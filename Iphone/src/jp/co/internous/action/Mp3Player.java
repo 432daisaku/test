2ab667bc55b24f99f453e2c8a41ccb398bef71b6
432daisaku <mystar.ds1sm@gmail.com>
2019-11-06 21:23:39 +0900

    (11/6)

---
+++ Iphone/src/jp/co/internous/action/Mp3Player.java
@@ -0,0 +1,23 @@
+package jp.co.internous.action;
+
+public class Mp3Player {
+
+	public void play(){
+
+		System.out.println("Ä¶");
+
+	}
+	public void stop(){
+
+		System.out.println("’â~");
+	}
+	public void next(){
+
+		System.out.println("Ÿ‚Ö");
+	}
+	public void back(){
+
+		System.out.println("–ß‚é");
+	}
+
+}
