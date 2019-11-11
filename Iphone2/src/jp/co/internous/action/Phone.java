3c21117fc9a10090bc61c15d3537b555109eef87
432daisaku <mystar.ds1sm@gmail.com>
2019-11-11 21:30:39 +0900

    object

--- Iphone2/src/jp/co/internous/action/Phone.java
+++ Iphone2/src/jp/co/internous/action/Phone.java
@@ -1,23 +1,21 @@
 package jp.co.internous.action;
 
-public class SmartPhone extends Mp3Player{
-	public void play(){
-		System.out.println("再生(ハイレゾ機能)");
+public class Phone {
 
+	public void play(){
+		System.out.println("再生");
 	}
 	public void stop(){
-		System.out.println("停止(ハイレゾ機能)");
+		System.out.println("停止");
 	}
 	public void next(){
-		System.out.println("次へ(ハイレゾ機能)");
+		System.out.println("次へ");
 	}
 	public void back(){
-		System.out.println("戻る(ハイレゾ機能)");
+		System.out.println("戻る");
 	}
-
 	public void call(){
 		System.out.println("電話");
-
 	}
 	public void mail(){
 		System.out.println("メール");
@@ -27,7 +25,6 @@ public class SmartPhone extends Mp3Player{
 	}
 	public void internet(){
 		System.out.println("インターネット");
-
 	}
 
 }
---
+++ Iphone2/src/jp/co/internous/action/SmartPhone.java
@@ -0,0 +1,21 @@
+package jp.co.internous.action;
+
+public class SmartPhone extends Phone implements Mp3Player, NewFunction{
+
+//	public void play(){
+//		System.out.println("Ä¶");
+//	}
+//	public void stop(){
+//		System.out.println("’â~");
+//
+//	}
+//	public void next(){
+//
+//		System.out.println("Ÿ‚Ö");
+//
+//	}
+//	public void back(){
+//		System.out.println("–ß‚é");
+//	}
+
+}
